98e0c607f568740f596ef917668401ee52403e84
MostafaSaeedDeveloper <mostafasaeed.developer@gmail.com>
2026-02-25 23:19:05 +0200

    Restore events list layout and align event cards with homepage

--- resources/views/front/events/index.blade.php
+++ resources/views/front/events/index.blade.php
@@ -4,7 +4,7 @@
     <div class="sub-banner">
         <div class="container">
             <h6>TKT House Events</h6>
-            <p>Discover upcoming techno parties and reserve your ticket.</p>
+            <p>Book upcoming techno events with secure ticket access.</p>
         </div>
     </div>
 
@@ -14,28 +14,72 @@
                 <div class="msl-concert-list">
                     <figure><img src="extra-images/concert1.jpg" alt="Underground Pulse"></figure>
                     <div class="text-overflow">
-                        <h4 class="concert-title"><a href="{{ route('front.events.show') }}">Underground Pulse: Warehouse Edition</a></h4>
+                        <h4 class="concert-title">
+                            <a href="{{ route('front.events.show') }}">Underground Pulse: Warehouse Edition</a>
+                        </h4>
                         <div class="concert-meta">
-                            <div class="concert-info"><b>Date:</b> <span>22 March, 2026</span></div>
-                            <div class="concert-info"><b>Time:</b> <span>10:00 PM</span></div>
-                            <div class="concert-info concert-location"><b>Location:</b> <span>Cairo Warehouse District</span></div>
+                            <div class="concert-info">
+                                <b>Date:</b>
+                                <span>22 March, 2026</span>
+                            </div>
+                            <div class="concert-info">
+                                <b>Phone:</b>
+                                <span>+20 100 555 8899</span>
+                            </div>
+                            <div class="concert-info concert-location">
+                                <b>Location:</b>
+                                <span>Cairo Warehouse District, Egypt</span>
+                            </div>
                         </div>
-                        <p>A deep and driving techno experience with headline DJs, immersive lights, and full-night production.</p>
-                        <a class="btn-1" href="{{ route('front.events.show') }}">View Details</a>
+                        <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
                     </div>
                 </div>
 
                 <div class="msl-concert-list">
                     <figure><img src="extra-images/concert2.jpg" alt="Bassline Temple"></figure>
                     <div class="text-overflow">
-                        <h4 class="concert-title"><a href="{{ route('front.events.show') }}">Bassline Temple: Seafront Session</a></h4>
+                        <h4 class="concert-title">
+                            <a href="{{ route('front.events.show') }}">Bassline Temple: Seafront Session</a>
+                        </h4>
                         <div class="concert-meta">
-                            <div class="concert-info"><b>Date:</b> <span>12 April, 2026</span></div>
-                            <div class="concert-info"><b>Time:</b> <span>9:30 PM</span></div>
-                            <div class="concert-info concert-location"><b>Location:</b> <span>Alexandria Seafront Arena</span></div>
+                            <div class="concert-info">
+                                <b>Date:</b>
+                                <span>12 April, 2026</span>
+                            </div>
+                            <div class="concert-info">
+                                <b>Phone:</b>
+                                <span>+20 100 555 8899</span>
+                            </div>
+                            <div class="concert-info concert-location">
+                                <b>Location:</b>
+                                <span>Alexandria Seafront Arena, Egypt</span>
+                            </div>
                         </div>
-                        <p>Open-air vibes, curated lineups, and high-energy techno performances by regional and international artists.</p>
-                        <a class="btn-1" href="{{ route('front.events.show') }}">View Details</a>
+                        <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
+                    </div>
+                </div>
+
+                <div class="msl-concert-list">
+                    <figure><img src="extra-images/concert3.jpg" alt="Neon Frequency"></figure>
+                    <div class="text-overflow">
+                        <h4 class="concert-title">
+                            <a href="{{ route('front.events.show') }}">Neon Frequency: Open Air Dome</a>
+                        </h4>
+                        <div class="concert-meta">
+                            <div class="concert-info">
+                                <b>Date:</b>
+                                <span>30 April, 2026</span>
+                            </div>
+                            <div class="concert-info">
+                                <b>Phone:</b>
+                                <span>+20 100 555 8899</span>
+                            </div>
+                            <div class="concert-info concert-location">
+                                <b>Location:</b>
+                                <span>Giza Open Air Dome, Egypt</span>
+                            </div>
+                        </div>
+                        <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
                     </div>
                 </div>
             </div>
