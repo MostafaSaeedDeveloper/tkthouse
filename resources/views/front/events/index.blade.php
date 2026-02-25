052ad44ecbd99c4d716a0282ed03d37118f1b52d
MostafaSaeedDeveloper <mostafasaeed.developer@gmail.com>
2026-02-25 23:24:02 +0200

    Restore events page cards to original template layout

--- resources/views/front/events/index.blade.php
+++ resources/views/front/events/index.blade.php
@@ -1,34 +1,27 @@
 @extends('front.layout.master')
 
 @section('content')
-    <div class="sub-banner">
-        <div class="container">
-            <h6>TKT House Events</h6>
-            <p>Book upcoming techno events with secure ticket access.</p>
-        </div>
-    </div>
-
     <div class="kode_content_wrap">
         <section>
             <div class="container">
                 <div class="msl-concert-list">
-                    <figure><img src="extra-images/concert1.jpg" alt="Underground Pulse"></figure>
+                    <figure><img src="extra-images/concert1.jpg" alt="KODEFOREST"></figure>
                     <div class="text-overflow">
                         <h4 class="concert-title">
-                            <a href="{{ route('front.events.show') }}">Underground Pulse</a>
+                            <a href="{{ route('front.events.show') }}">Lorem Ipsum Proin gravida nibh vel velit auctor aliquet</a>
                         </h4>
                         <div class="concert-meta">
                             <div class="concert-info">
                                 <b>Date:</b>
-                                <span>22 March, 2026</span>
+                                <span>25 July, 2018</span>
                             </div>
                             <div class="concert-info">
                                 <b>Phone:</b>
-                                <span>+20 100 555 8899</span>
+                                <span>06 511 21022</span>
                             </div>
                             <div class="concert-info concert-location">
                                 <b>Location:</b>
-                                <span>Cairo Warehouse District</span>
+                                <span>Level 13, 2 Elizabeth St, Melbourne Victoria 3000 Australia</span>
                             </div>
                         </div>
                         <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
@@ -36,23 +29,23 @@
                 </div>
 
                 <div class="msl-concert-list">
-                    <figure><img src="extra-images/concert2.jpg" alt="Bassline Temple"></figure>
+                    <figure><img src="extra-images/concert2.jpg" alt="KODEFOREST"></figure>
                     <div class="text-overflow">
                         <h4 class="concert-title">
-                            <a href="{{ route('front.events.show') }}">Bassline Temple</a>
+                            <a href="{{ route('front.events.show') }}">Lorem Ipsum Proin gravida nibh vel velit auctor aliquet</a>
                         </h4>
                         <div class="concert-meta">
                             <div class="concert-info">
                                 <b>Date:</b>
-                                <span>12 April, 2026</span>
+                                <span>25 July, 2018</span>
                             </div>
                             <div class="concert-info">
                                 <b>Phone:</b>
-                                <span>+20 100 555 8899</span>
+                                <span>06 511 21022</span>
                             </div>
                             <div class="concert-info concert-location">
                                 <b>Location:</b>
-                                <span>Alexandria Seafront Arena</span>
+                                <span>Level 13, 2 Elizabeth St, Melbourne Victoria 3000 Australia</span>
                             </div>
                         </div>
                         <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
@@ -60,23 +53,23 @@
                 </div>
 
                 <div class="msl-concert-list">
-                    <figure><img src="extra-images/concert3.jpg" alt="Neon Frequency"></figure>
+                    <figure><img src="extra-images/concert3.jpg" alt="KODEFOREST"></figure>
                     <div class="text-overflow">
                         <h4 class="concert-title">
-                            <a href="{{ route('front.events.show') }}">Neon Frequency</a>
+                            <a href="{{ route('front.events.show') }}">Lorem Ipsum Proin gravida nibh vel velit auctor aliquet</a>
                         </h4>
                         <div class="concert-meta">
                             <div class="concert-info">
                                 <b>Date:</b>
-                                <span>30 April, 2026</span>
+                                <span>25 July, 2018</span>
                             </div>
                             <div class="concert-info">
                                 <b>Phone:</b>
-                                <span>+20 100 555 8899</span>
+                                <span>06 511 21022</span>
                             </div>
                             <div class="concert-info concert-location">
                                 <b>Location:</b>
-                                <span>Giza Open Air Dome</span>
+                                <span>Level 13, 2 Elizabeth St, Melbourne Victoria 3000 Australia</span>
                             </div>
                         </div>
                         <a class="btn-1 theme-bg" href="{{ route('front.events.show') }}">Buy Ticket</a>
