0e9aba423352e451ce7ac8460ef98d93087c7c9a
MostafaSaeedDeveloper <mostafasaeed.developer@gmail.com>
2026-03-17 00:46:05 +0200

    Add Google Analytics gtag snippet to front header

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -12,6 +12,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2321WHVMSX"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2321WHVMSX');
+        </script>
         <title>{{ $siteName }} | Techno Events & Ticket Booking</title>
         <!-- Bootstrap core CSS -->
         <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
